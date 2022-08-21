4e8fb75d4f04b79feaaf7e1dc55ad7b7cc9dcc18
eonarga <assisneto013@gmail.com>
2022-08-21 18:39:44 -0400

    first commit

---
+++ public/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Cadastro de cliente</title>
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+</head>
+
+<body>
+  <div class="card m-3">
+    <div class="card-header">
+      Cadastro de cliente
+    </div>
+    <div class="card-body">
+    <div class="alert alert-primary" role="alert">
+ Os campos com * são obrigatórios.
+</div>
+<div class="mb-3"> 
+  <label for="exampleFormControlInput1" class="form-label">Nome*</label>
+  <input type="Nome" class="form-control" id="exampleFormControlInput1" placeholder="Insira seu nome...">
+</div>
+<div class="mb-3">
+  <label for="exampleFormControlInput1" class="form-label">Sobre nome*</label>
+  <input type="Sobre nome" class="form-control" id="exampleFormControlInput1" placeholder="Insira seu sobre nome...">
+</div>
+    </div>
+    <div class="card-footer">
+      <button class="btn btn-success">Salvar</button>
+    </div>
+  </div>
+  <script src="js/bootstrap.min.js"></script>
+</body>
+
+</html>
